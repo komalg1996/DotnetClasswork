f6611283b9cff5a650aec91e03101b3c281c1e70
dac <dac@dypm.org>
2023-01-05 16:03:08 +0530

    addLINQ

---
+++ Day7LINQ/ExtensionApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Mathematics;
+
+MathEngine  m1= new MathEngine();
+int result = m1.Addition(10,20);
+Console.WriteLine("Add result ="+result);
+result = m1.Subtraction(20,5);
+Console.WriteLine("Sub result ="+result);
+result =m1.Multiply(10,20);
+Console.WriteLine("Multiply result ="+result);
+result=m1.Divide(50,10);
+Console.WriteLine("Divide ="+result);
+
